bc7a179aaa22eced67224bf7835bab857dc93ff1
Shafay Hussain <shafay.pro98@gmail.com>
2026-09-25 10:15:24 +0500

    removed global presence from home page

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -31,7 +31,6 @@ const HomePage = () => {
         <HowWeWorkSection />
         <WhyITGSection />
         <InsightsSection />
-        <GlobalPresenceSection />
         <FinalCTASection />
       </main>
 
